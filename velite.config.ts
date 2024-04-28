8fadced87e48903123d8d3b0401d15ba656888ee
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-04-28 18:57:25 +0300

    changed display theme for code in mdx files

--- velite.config.ts
+++ velite.config.ts
@@ -1,27 +1,29 @@
 import { defineConfig, defineCollection, s } from "velite"
-import rehypeSlug from "rehype-slug"
-import rehypePrettyCode from "rehype-pretty-code"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
+
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const computedFields = <T extends { slug: string }>(data: T) => ({
     ...data,
     slugAsParams: data.slug.split("/").slice(1).join("/"),
-})
+});
 
 const posts = defineCollection({
     name: "Post",
     pattern: "blog/**/*.mdx",
     schema: s
-        .object({
-            slug: s.path(),
-            title: s.string().max(99),
-            description: s.string().max(999).optional(),
-            date: s.isodate(),
-            published: s.boolean().default(true),
-            body: s.mdx(),
-    })
-    .transform(computedFields),
-});
+      .object({
+        slug: s.path(),
+        title: s.string().max(99),
+        description: s.string().max(500).optional(),
+        date: s.isodate(),
+        published: s.boolean().default(true),
+        tags: s.array(s.string()).optional(),
+        body: s.mdx(),
+      })
+      .transform(computedFields),
+}); 
 
 export default defineConfig({
     root: "content",
@@ -34,19 +36,8 @@ export default defineConfig({
     },
     collections: { posts },
     mdx: {
-      rehypePlugins: [
-        rehypeSlug,
-        [rehypePrettyCode, { theme: "github-dark" }],
-        [rehypeAutolinkHeadings,
-                                {
-                                    behavior: "wrap",
-                                    properties: {
-                                    className: ["subheading-anchor"],
-                                    ariaLabel: "Link to section",
-                                    },
-                                },
-        ],
-      ],
+      rehypePlugins: [rehypeSlug, [rehypePrettyCode, {theme:"one-dark-pro"}], 
+                     [rehypeAutolinkHeadings, {behavior:"wrap", properties:{className:["subheading-anchor"], ariaLabel:"Link to section",}}]],
       remarkPlugins: [],
     },
-  });
+});
