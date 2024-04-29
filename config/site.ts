2d39dcd52944fabec6d6e996a5449f8adc21e701
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-04-29 19:05:58 +0300

    Create site.ts

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "DigitalNotebook",
+    url: "https://example.com",
+    description: "Educational blog",
+    author1: "Myriam",
+    links: {
+      github: "https://github.com/myrmlbst",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
