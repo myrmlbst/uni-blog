0c8c7c396d545006a391a965fbd6c61d0af00b5e
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-05-01 23:55:10 +0300

    Updated Components file

---
+++ components/main-nav.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export function MainNav() {
+    const pathname = usePathname();
+    return (
+      <nav className="flex items-center space-x-4 lg:space-x-6">
+        <Link href="/" className="mr-6 flex items-center space-x-2">
+          <span className="font-bold">{siteConfig.name}</span>
+        </Link>
+        <Link
+          href="/blog"
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+            pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Blog
+        </Link>
+        <Link
+          href="/about"
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+            pathname === "/about" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          About
+        </Link>
+      </nav>
+    );
+  }
---
+++ components/mdx-components.tsx
@@ -0,0 +1,25 @@
+import * as runtime from "react/jsx-runtime";
+
+import Image from "next/image";
+
+import { Callout } from "./callout";
+
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const components = {
+  Image,
+  Callout,
+};
+
+interface MdxProps {
+  code: string;
+}
+  
+export function MDXContent({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+  return <Component components={components} />;
+}
+  
