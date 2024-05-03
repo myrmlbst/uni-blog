b23a32f3123b9f351511a5a599031cca8051860c
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-05-03 17:18:13 +0300

    Updated About page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export default async function AboutPage() {
+  return (
+    <div className="container max-w-6xl py-6 lg:py-10">
+      <h1 className="inline-block font-black text-4xl lg:text-5xl">
+        About Me
+      </h1>
+
+      <p className="text-muted-foreground text-lg py-4">
+        Hey there, I'm Myriam! I'm a computer science student with a passion for all things tech-related.
+        <br /><br />
+        While studying during my undergrad, I watched my knowledge expand endlessly; the more I learned, the more I knew I needed a place to keep track of all the knowledge I've acquired over the years.
+        And that is how this blog was born! I'm using this site to upload and keep track of all the things I've learned within the past few years, and share them with other students who might also be in need.
+        <br/><br/>
+        I've also left the link to the source code in my GitHub repository to all those interested in web development to see how this blog was made.
+     </p>
+    </div>
+  )
+}
